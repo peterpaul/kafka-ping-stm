4ffe404bcdc808a8a61473cb7a4dff3df6e037cf
Peterpaul Klein Haneveld <pp.kleinhaneveld@gmail.com>
2022-08-05 16:16:49 +0200

    Send ping and receive pong using kafka

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use kafka::consumer::{Consumer, FetchOffset};
+use kafka::producer::{Producer, Record, RequiredAcks};
 use log::debug;
 use oblivious_state_machine::{
     state::{DeliveryStatus, State, StateTypes, Transition},
     state_machine::{TimeBoundStateMachineResult, TimeBoundStateMachineRunner},
 };
+use serde::{Deserialize, Serialize};
 use std::collections::VecDeque;
 use std::time::Duration;
 use tokio::{select, time};
@@ -10,7 +13,7 @@ use tokio::{select, time};
 #[derive(Debug)]
 enum IncomingMessage {
     SendPing,
-    Pong,
+    Pong(Pong),
 }
 
 #[derive(Debug)]
@@ -21,18 +24,44 @@ impl StateTypes for Types {
     type Err = String;
 }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
+struct Ping {
+    id: u32,
+}
+
+impl Ping {
+    fn new() -> Self {
+        Self { id: 42 }
+    }
+}
+
 struct SendingPing {
     ping_sent: bool,
+    producer: Producer,
 }
 
 impl SendingPing {
     fn new() -> Self {
-        Self { ping_sent: false }
+        let producer = Producer::from_hosts(vec!["localhost:9092".to_owned()])
+            .with_ack_timeout(Duration::from_secs(1))
+            .with_required_acks(RequiredAcks::One)
+            .create()
+            .unwrap();
+
+        Self {
+            ping_sent: false,
+	    producer,
+        }
     }
 
     fn send_ping(&mut self) {
         debug!("Send Ping");
+        let ping = Ping::new();
+        let ping_message = serde_json::to_string_pretty(&ping).expect("json serialization failed");
+        let ping_record = Record::from_value("ping", ping_message);
+        self.producer
+            .send(&ping_record)
+            .expect("failed to send message");
         self.ping_sent = true;
     }
 }
@@ -65,6 +94,11 @@ impl State<Types> for SendingPing {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Pong {
+    id: u32,
+}
+
 #[derive(Debug)]
 struct ListeningForPong {
     pong_received: bool,
@@ -77,9 +111,9 @@ impl ListeningForPong {
         }
     }
 
-    fn receive_pong(&mut self) {
+    fn receive_pong(&mut self, pong: Pong) {
+        debug!("Received Pong: {}", pong.id);
         self.pong_received = true;
-        debug!("Received Pong");
     }
 }
 
@@ -93,8 +127,8 @@ impl State<Types> for ListeningForPong {
         message: IncomingMessage,
     ) -> DeliveryStatus<IncomingMessage, <Types as StateTypes>::Err> {
         match message {
-            IncomingMessage::Pong => {
-                self.receive_pong();
+            IncomingMessage::Pong(pong) => {
+                self.receive_pong(pong);
                 DeliveryStatus::Delivered
             }
             _ => DeliveryStatus::Unexpected(message),
@@ -115,7 +149,14 @@ impl State<Types> for ListeningForPong {
 async fn main() {
     pretty_env_logger::init();
 
-    let mut feed = VecDeque::from([IncomingMessage::SendPing, IncomingMessage::Pong]);
+    let mut consumer = Consumer::from_hosts(vec!["localhost:9092".to_owned()])
+        .with_topic("pong".to_owned())
+        .with_fallback_offset(FetchOffset::Earliest)
+        .with_group("my_consumer_group".to_owned())
+        .create()
+        .expect("invalid consumer config");
+
+    let mut feed = VecDeque::from([IncomingMessage::SendPing]);
     let state: Box<dyn State<Types> + Send> = Box::new(SendingPing::new());
 
     let mut feeding_interval = time::interval(Duration::from_millis(100));
@@ -127,6 +168,14 @@ async fn main() {
     let (_outgoing, mut result) = state_machine_runner.run();
 
     let res: TimeBoundStateMachineResult<Types> = loop {
+        for msg_result in consumer.poll().unwrap().iter() {
+            for msg in msg_result.messages() {
+                // let _key: &str = std::str::from_utf8(msg.key).unwrap();
+                let pong: Pong =
+                    serde_json::from_slice(msg.value).expect("failed to deser JSON to Pong");
+                feed.push_front(IncomingMessage::Pong(pong));
+            }
+        }
         select! {
             res = &mut result => {
                 break res.expect("Result from State Machine must be communicated");
